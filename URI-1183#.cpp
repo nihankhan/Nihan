557b0eaab362519303cf8af08998de53c174b832
NIhan Khan <nihan.khan@outlook.com>
2021-03-02 09:54:24 +0600

    Add files via upload
    This is a Solution of URI-1183 Problem.
    
    
    https://www.urionlinejudge.com.br/judge/en/problems/view/1183

---
+++ URI-1183#.cpp
@@ -0,0 +1,55 @@
+/*
+ *******************************************
+ *      * AUTHOR : Nihan_Khan *            *
+ *      * NICK :   NullByte      *         *
+ *      * CREATED: 01.03.2021 12:24 *      *
+ *******************************************
+*/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    vector<vector<double>> vec(3);
+    char x;
+    double n, sum = 0.0;
+
+    cin >> x;
+
+    for (int i = 0; i < vec.size(); i++)
+    {
+    	for (int j = 0; j < vec.size(); j++)
+    	{
+    		cin >> n;
+
+    		vec[i].push_back(n);
+
+    		//cout << "i = " << i << endl;
+    		//cout << "j = " << j << endl;
+
+    		
+    		if (i > j)
+    		{
+    			sum += vec[i][j];
+
+    			//cout << "i = " << i << endl;
+    			//cout << "j = " << j << endl;
+    		}
+    	}
+    }
+
+    if (x == 'S')
+    {
+    	printf("%.1lf\n", sum);
+    }
+    else
+    {
+    	printf("%.1lf\n", sum/66.0);
+    }
+
+    return 0;
+}
